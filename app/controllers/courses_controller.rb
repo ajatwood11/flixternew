2cf6779c1b71fbe07e2c4679b60c26d5dc8df23f
Austin Atwood <austinatwoodjs30@gmail.com>
2019-12-24 05:42:57 +0000

    upload

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,23 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost, :image)
+  end
+end
