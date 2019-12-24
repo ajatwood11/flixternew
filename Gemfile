cde6afff654ad53f5453ff34dcfb89151acaf23f
Austin Atwood <austinatwoodjs30@gmail.com>
2019-12-24 05:41:53 +0000

    new

--- Gemfile
+++ Gemfile
@@ -68,5 +68,6 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
-
-
+gem 'simple_form'
+gem 'devise'
+gem 'carrierwave', '~> 2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_073239) do
+ActiveRecord::Schema.define(version: 2019_12_24_053427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,13 +22,7 @@ ActiveRecord::Schema.define(version: 2019_12_17_073239) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
-  end
-
-  create_table "enrollments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "lessons", force: :cascade do |t|
@@ -37,7 +31,6 @@ ActiveRecord::Schema.define(version: 2019_12_17_073239) do
     t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "video"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
