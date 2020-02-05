be4e3fbe490cc015fbbe303244bed11522e7c302
Austin Atwood <austinatwoodjs30@gmail.com>
2020-02-05 02:22:18 +0000

    lastest

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 2020_02_05_011718) do
     t.text "description"
     t.decimal "cost"
     t.integer "user_id"
-    t.integer "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
